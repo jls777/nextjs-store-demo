18e90a32acee10489e8c2b50dd399fb885e2555e
John Sison <132543192+jls777@users.noreply.github.com>
2025-03-04 01:10:55 -0500

    installed components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 function HomePage() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <div>Homepage</div>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        click me
+      </Button>
+    </div>
+  );
 }
 
 export default HomePage;
