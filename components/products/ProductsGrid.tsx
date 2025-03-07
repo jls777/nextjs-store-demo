5bdbacd62f5cc47208901f2738c0d98585862f6d
John Sison <132543192+jls777@users.noreply.github.com>
2025-03-07 00:20:23 -0500

    hero component

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -14,43 +14,16 @@ function ProductsGrid({ products }: { products: Product[] }) {
         const dollarsAmount = formatCurrency(price);
 
         return (
-          // <article key={productId} className="group relative">
-          //   <Link href={`/product/${productId}`}>
-          //     <Card className="transformation group-hover:shadow-xl transition-shadow duration-500">
-          //       <CardContent className="p-4">
-          //         <div className="relative h-64 md:h-48 rounded overflow-hidden">
-          //           <Image
-          //             src={image}
-          //             alt={name}
-          //             fill
-          //             sizes="(max-width:768px) 100vw,(max-width:1200px) 50vw, 33vw"
-          //             priority
-          //             className="rounded w-full object-cover transform group-hover:scale-110 transition-transform duration-500"
-          //           />
-          //         </div>
-          //         <div className="mt-4 text-center">
-          //           <h2 className="text-lg capitalize">{name}</h2>
-          //           <p className="text-muted-foreground mt-2">
-          //             {dollarsAmount}
-          //           </p>
-          //         </div>
-          //       </CardContent>
-          //     </Card>
-          //   </Link>
-          //   <div className="absolute top-7 right-7 z-5">
-          //     <FavoriteToggleButton productId={productId} />
-          //   </div>
-          // </article>
           <article key={productId} className="group relative">
-            <Link href={`/products/${productId}`}>
-              <Card className="transform group-hover:shadow-xl transition-shadow duration-500">
+            <Link href={`/product/${productId}`}>
+              <Card className="transformation group-hover:shadow-xl transition-shadow duration-500">
                 <CardContent className="p-4">
-                  <div className="relative h-64 md:h-48 rounded overflow-hidden ">
+                  <div className="relative h-64 md:h-48 rounded overflow-hidden">
                     <Image
                       src={image}
                       alt={name}
                       fill
-                      sizes="(max-width:768px) 100vw,(max-width:1200px) 50vw, 33vw "
+                      sizes="(max-width:768px) 100vw,(max-width:1200px) 50vw, 33vw"
                       priority
                       className="rounded w-full object-cover transform group-hover:scale-110 transition-transform duration-500"
                     />
