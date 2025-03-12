3ddd767c8516094d143dec8a2a1b23917c229ae3
John Sison <132543192+jls777@users.noreply.github.com>
2025-03-12 14:16:39 -0400

    productrating component

--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -1,5 +1,17 @@
-function ProductRating() {
-  return <div>product rating</div>;
+import { FaStar } from "react-icons/fa";
+
+async function ProductRating({ productId }: { productId: string }) {
+  const rating = 4.2;
+  const count = 25;
+
+  const className = `flex gap-1 items-center text-md mt-1 mb-4`;
+  const countValue = `(${count}) reviews`;
+  return (
+    <span className={className}>
+      <FaStar className="w-3 h-3" />
+      {rating} {countValue}
+    </span>
+  );
 }
 
 export default ProductRating;
