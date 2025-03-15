632a0d85f5a063e468440c9ba993eb77b6549183
John Sison <132543192+jls777@users.noreply.github.com>
2025-03-15 12:57:55 -0400

    signout link component

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,5 +1,20 @@
+"use client";
+
+import { toast } from "sonner";
+import { SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
+
 function SignOutLink() {
-  return <div>signoutlink</div>;
+  const handleLogout = () => {
+    toast("Logout Successful");
+  };
+  return (
+    <SignOutButton>
+      <Link href="/" className="w-full text-left" onClick={handleLogout}>
+        Logout
+      </Link>
+    </SignOutButton>
+  );
 }
 
 export default SignOutLink;
