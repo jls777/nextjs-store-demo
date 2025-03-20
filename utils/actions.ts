3fb552e1e0c4aa76c1f103ca3baab917b592a243
John Sison <132543192+jls777@users.noreply.github.com>
2025-03-20 00:02:54 -0400

    createproductionaction

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,23 @@
+"use server";
+
 import db from "@/utils/db";
+import { currentUser, auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : "An error occurred",
+  };
+};
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("You must be logged in to access this route");
+  }
+  return user;
+};
 
 export const fetchFeaturedProducts = async () => {
   const products = await db.product.findMany({
@@ -11,7 +29,7 @@ export const fetchFeaturedProducts = async () => {
   return products;
 };
 
-export const fetchAllProducts = ({ search = "" }: { search: string }) => {
+export const fetchAllProducts = async ({ search = "" }: { search: string }) => {
   return db.product.findMany({
     where: {
       OR: [
@@ -33,4 +51,35 @@ export const fetchSingleProduct = async (productId: string) => {
   });
   if (!product) redirect("/products");
   return product;
+};
+
+export const createProductAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+  try {
+    const name = formData.get("name") as string;
+    const company = formData.get("company") as string;
+    const price = Number(formData.get("price") as string);
+    const image = formData.get("image") as File;
+    const description = formData.get("description") as string;
+    const featured = Boolean(formData.get("featured") as string);
+
+    await db.product.create({
+      data: {
+        name,
+        company,
+        price,
+        image: "/images/product-1.jpg",
+        description,
+        featured,
+        clerkId: user.id,
+      },
+    });
+
+    return { message: "product created" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
