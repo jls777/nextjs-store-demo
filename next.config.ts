2ed482a788ce4a8a9a01d501a4f1521cc4a5bdfe
John Sison <132543192+jls777@users.noreply.github.com>
2025-03-31 05:19:15 -0400

    product review component

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "vbuyaxvfxkfyptlgzmzo.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
     ],
   },
 };
